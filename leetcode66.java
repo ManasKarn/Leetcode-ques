93abd6a6aea758f30d1b05230df77eaf5b934833
ManasKarn <manaskarn2007@gmail.com>
2025-10-24 22:43:52 +0530

    Ques no 66

--- leetcode66.java
+++ leetcode66.java
@@ -1,36 +1,21 @@
-import java.util.ArrayList;
 
-class Solution {
+class Solution66 {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int i = digits.length - 1;
-        if (digits[i] != 9) {
-            digits[i] = digits[i] + 1;
-            return digits;
-        } else {
-            while (i >= 0 && digits[i] == 9) {
-                list.addLast(0);
-                i--;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
             }
+            digits[i] = 0;
         }
-        if (i != -1) {
-            digits[i] = digits[i] + 1;
-        } else {
-            list.addFirst(1);
-        }
-        for (; i >= 0; i--) {
-            list.addFirst(digits[i]);
-        }
-        int ar[] = new int[list.size()];
-        for (i = 0; i < ar.length; i++) {
-            ar[i] = list.get(i);
-        }
-        return ar;
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 
     public static void main(String[] args) {
-        Solution ob = new Solution();
-        int ar[] = { 1, 0, 9, 0, 9 };
+        Solution66 ob = new Solution66();
+        int ar[] = { 9, 9, 9 };
         ar = ob.plusOne(ar);
         for (int a = 0; a < ar.length; a++)
             System.out.println(ar[a]);
